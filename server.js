b21671ed0747243a39cda2169c5249c60c476d22
Hunter Livesay <livesayme@gmail.com>
2022-04-22 17:10:44 -0600

    Added routes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use('/', require('./routes'));
 
 app.listen(port, () => {
