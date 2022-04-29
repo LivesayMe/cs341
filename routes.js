d406aeca54be19766ec416fa8d34d679f12f498c
Hunter Livesay <livesayme@gmail.com>
2022-04-29 14:46:59 -0600

    Added mongodb funcitonality

--- routes.js
+++ routes.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const mongodb = require('./mongodb');
+var mongo = require('mongodb');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.send('Anne Harley');
 });
 
+router.get('/contacts', async function(req, res, next) {
+  const results = await mongodb.getDb().collection('contacts').find().toArray();
+  res.send(results);
+});
+
+router.get('/contact', async function(req, res, next) {
+  console.log(req.query.id);
+  const results = await mongodb.getDb().collection('contacts').findOne({"_id": new mongo.ObjectId(req.query.id)});
+  res.send(results);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const mongodb = require('./mongodb');
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+require('dotenv').config();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+mongodb.initDB((err, mongodb) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
 })
